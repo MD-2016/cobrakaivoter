e279cf24029cd0145e3c8baf421236ddefe9f3d0
MD-2016 <programforcoffee89@gmail.com>
2022-09-18 21:07:36 -0400

    switched to chakra ui for styling

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,17 +1,35 @@
-// src/pages/_app.tsx
+import { ChakraProvider } from "@chakra-ui/react";
+import { Button } from '@chakra-ui/react';
+import { ColorModeScript, useColorMode } from '@chakra-ui/react';
 import { withTRPC } from "@trpc/next";
 import type { AppRouter } from "../server/router";
 import type { AppType } from "next/dist/shared/lib/utils";
 import superjson from "superjson";
 import "../styles/globals.css";
 import SideBar from "../components/navbar";
+import { AppProps } from "next/app";
+import React from "react";
 
-const MyApp: AppType = ({ Component, pageProps }) => {
-  <div>
-    <SideBar />
-  </div>
-  return <Component {...pageProps} />;
-};
+function MyApp({ Component, pageProps }: AppProps) {
+  const desc = "Pick the best fighter in the Cobra Kai and Karate Kid Verse";
+  const title = "Best KK and CK fighter poll site";
+  const imageMeta = "https://thekaratekid.fandom.com/wiki/Cobra_Kai_Dojo?file=Cobra_kai_logo_vector.svg";
+  const { colorMode, toggleColorMode } = useColorMode();
+
+
+  return (
+    <ChakraProvider>
+      <div>
+        <Button onClick={toggleColorMode}>
+          Toggle Mode {colorMode}
+        </Button>
+        <SideBar />
+        <Component {...pageProps} />
+      </div>
+    </ChakraProvider>
+  );
+
+}
 
 const getBaseUrl = () => {
   if (typeof window !== "undefined") return ""; // browser should use relative url
--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,4 +1,3 @@
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 const AboutPage = () => {
     return (
         <div className="flex flex-col items-center text-xl">
