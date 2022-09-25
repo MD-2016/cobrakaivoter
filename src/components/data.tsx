f90b9f0087b08670b85e76055fb8f8f35e99fbf3
MD-2016 <programforcoffee89@gmail.com>
2022-09-25 19:06:19 -0400

    Finished creating page info to pull from JSON on each fighter.

--- src/components/data.tsx
+++ src/components/data.tsx
@@ -1,11 +1,13 @@
 export const links = {
     "EFK": {
         "sensei": {
-            "ID": 5,
-            "bio": "The two time former All Valley champion who reopened Cobra Kai and lead his students to victory in the All Valley. He later lost his dojo and opened a new one under the new name Eagle Fang Karate. He would merge his students with Daniel LaRusso's Miyagi-Do Karate to take on Cobra Kai under John Kreese and Terry Silver.",
-            "name": "Johnny Lawrence",
-            "rivals": "John Kreese, Terry Silver",
-            "friends": "Bobby Brown, Dutch, Jimmy, Tommy, Miguel Diaz, Daniel LaRusso, Chozen Toguchi, Miyagi-Do fighters, Eagle Fang fighters",
+            "Johnny": {
+                "ID": 5,
+                "bio": "The two time former All Valley champion who reopened Cobra Kai and lead his students to victory in the All Valley. He later lost his dojo and opened a new one under the new name Eagle Fang Karate. He would merge his students with Daniel LaRusso's Miyagi-Do Karate to take on Cobra Kai under John Kreese and Terry Silver.",
+                "name": "Johnny Lawrence",
+                "rivals": "John Kreese, Terry Silver",
+                "friends": "Bobby Brown, Dutch, Jimmy, Tommy, Miguel Diaz, Daniel LaRusso, Chozen Toguchi, Miyagi-Do fighters, Eagle Fang fighters",
+            },
         },
         "students": {
             "Miguel": {
@@ -36,15 +38,167 @@ export const links = {
                 "rivals": "Terry Silver, Kyler Park, Kim Da-Eun",
                 "friends": "Johnny Lawrence, Tory Nichols, Bert, Nathaniel",
             },
+            "Aisha": {
+                "ID": 25,
+                "bio": "Close friend of Samantha and ends up joining Cobra Kai under Lawrence and competed in the 2018 All Valley. Later on, moves away from the valley. She could possibly join Eagle Fang because she uses the lessons Johnny Lawrence has taught her in her own life.",
+                "name": "Aisha Robinson",
+                "rivals": "Robby Keene, Chris",
+                "friends": "Samantha LaRusso, Daniel LaRusso, Miguel Diaz, Eli Moskowitz, Tory Nichols",
+            },
         },
-        "CK": {
-            "students": {
-                ""
-            }
+    },
+    "CK": {
+        "sensei": {
+            "John": {
+                "ID": 11,
+                "bio": "The original founder of the Cobra Kai dojo with friend Terry Silver. Kreese would run the dojo creating a dominate win the All Valley tournaments in the 80s which included two time All Valley champ Johnny Lawrence. After losing to Miyagi and Daniel LaRusso, he would return in Cobra Kai season 2 to eventually take over Cobra Kai from Johnny Lawrence and end up teaming up with Terry Silver. Kreese eventually gets booted from the dojo by Silver.",
+                "name": "John Kreese",
+                "rivals": "Miyagi, Daniel LaRusso, Johnny Lawrence, Terry Silver, Miyagi-Do, Eagle Fang",
+                "friends": "Kim Sun-Yung, Tory Nichols",
+            },
+            "Terry": {
+                "ID": 12,
+                "bio": "The wealthy and corrupt friend of John Kreese who helped found the Cobra Kai dojo. Would later assist Kreese in taking on Miyagi and Daniel LaRusso but failed in the 80s. Ends up rejoining Kreese in 2019 to help Cobra Kai win the All Valley and later takes over the dojo from Kreese.",
+                "name": "Terry Silver",
+                "rivals": "Daniel LaRusso, Miyagi, Johnny Lawrence, John Kreese, Miguel Diaz, Mike Barnes, Chozen Toguchi, Miyagi-Do, Eagle Fang",
+                "friends": "Kim Sun-Yung, Kim Da-Eun",
+            },
+            "Kim": {
+                "ID": 13,
+                "bio": "Granddaughter of the originator of the style that would become Cobra Kai named Kim Sun-Yung. Joins Terry Silver in season 5 to help Cobra Kai enter the Sekai Takai.",
+                "name": "Kim Da-Eun",
+                "rivals": "Tory Nichols, Devon Lee, Miyagi-Do, Eagle Fang",
+                "friends": "Kim Sun-Yung, John Kreese, Terry Silver, Cobra Kai",
+            },
+            "KimSun": {
+                "ID": 14,
+                "bio": "Creator of what would become the Cobra Kai style of karate, a deceptive form of Tang Soo Do. He would train his granddaughter Kim Da-Eun in this style and would teach John Kreese and Terry Silver.",
+                "name": "Kim Sun-Yung",
+                "rivals": "Sato Toguchi",
+                "friends": "Kim Da-Eun, John Kreese, Terry Silver",
+            },
         },
-        "MD": {
-            "students": {
+        "students": {
+            "Tory": {
+                "ID": 6,
+                "bio": "Originally appeared during Season 2 and joined Johnny's Cobra Kai. She stuck with Cobra Kai and one the 2019 All Valley under John Kreese and Terry Silver. A rival to Samantha LaRusso.",
+                "name": "Tory Nichols",
+                "rivals": "Samanatha LaRusso, Kim Da-Eun, Terry Silver, Miyagi-Do, Eagle Fang",
+                "friends": "Robby Keene, Devon Lee, Kyler Park, Kenny Payne, John Kreese",
+            },
+            "Kyler": {
+                "ID": 7,
+                "bio": "Rich kid bully who picks on lower status students. An athlete in wrestling and later joins Cobra Kai during season 3. He competed in the 2019 All Valley losing to Eli",
+                "name": "Kyler Park",
+                "rivals": "Miguel Diaz, Eli Moskowitz, Demetri Alexopoulos, Johnny Lawrence, Robby Keene, Terry Silver",
+                "friends": "Edwin, Tory Nichols, Kenny Payne, Terry Silver, Piper Elswith",
+            },
+            "Kenny": {
+                "ID": 8,
+                "bio": "Younger brother of Shawn Payne who joined Cobra Kai after being recruited by Robby Keene. Lost against Robby in the 2019 All Valley and later helped Cobra Kai secure a spot in the Senkai Taikai.",
+                "name": "Kenny Payne",
+                "rivals": "Anthony LaRusso, Bert, Nathaniel, Miguel Diaz, Eli Moskowitz, Robby Keene (s5)",
+                "friends": "Kyler Park, John Kreese, Terry Silver, Tory Nichols",
+            },
+            "Edwin": {
+                "ID": 9,
+                "bio": "Close friend of Kyler Park and previously Eli. Joined Cobra Kai under Johnny Lawrence and continued along with the change of sensei to John Kreese and Terry Silver. Competed in both All Valley tournaments.",
+                "name": "Edwin",
+                "rivals": "Demetri Alexopoulos, Samantha LaRusso, Eli Moskowitz, Miyagi-Do, Eagle Fang",
+                "friends": "Kyler Park, John Kreese, Terry Silver, Big Red, Stingray, Tory Nichols, Kenny Payne",
+            },
+            "Piper": {
+                "ID": 10,
+                "bio": "Former girlfriend of Moon who later joins Cobra Kai under sensei's John Kreese and Terry Silver. Competed in the All Valley 2019 losing to Samantha LaRusso",
+                "name": "Piper Elswith",
+                "rivals": "Samantha LaRusso, Miyagi-Do, Eagle Fang",
+                "friends": "Kyler Park, John Kreese, Terry Silver, Cobra Kai",
+            },
 
-            }
-        }
-    }
+        },
+    },
+    "MD": {
+        "sensei": {
+            "Miyagi": {
+                "ID": 15,
+                "bio": "Learned the Miyagi style of karate from his father where it is passed down from generation to generation. Would later move away from Okinawa (due to conflict with Sato) to live in the US and would enter WWII and earn the medal of honor. After losing his wife and child, Miyagi ended up eventually becoming the handyman for an apartment complex that Daniel LaRusso moved into. He would eventually teach Daniel LaRusso his karate which would help him win the All Valley twice against John Kreese's Cobra Kai. Sadly, Miyagi would pass away at age 86 on November 15th, 2011.",
+                "name": "Nariyoshi Miyagi",
+                "rivals": "Mike Barnes, Terry Silver, John Kreese, Johnny Lawrence, Paul Dugan",
+                "friends": "Daniel LaRusso, Sato Yoguchi",
+            },
+            "Sato": {
+                "ID": 16,
+                "bio": "Miyagi's best friend who was taught Miyagi's style of karate from Miyagi's father after Miyagi got permission. After a falling out with Miyagi over a love interest, Sato would open a dojo to teach US Military the Miyagi style of karate. He would teach his nephew Chozen the Miyagi style as well. After his life was saved by Miyagi during a typhoon, Sato would befriend Miyagi and drop his grudge.",
+                "name": "Sato Toguchi",
+                "rivals": "Kim Sun-Yung",
+                "friends": "Miyagi, Chozen Toguchi, Daniel LaRusso",
+            },
+            "Chozen": {
+                "ID": 17,
+                "bio": "The nephew to Sato who was taught Miyagi style of karate and would end up fighting Daniel to the death. After his defeat from Daniel, he would later teach Daniel the Miyagi secret pressure points and eventually assist Daniel in training Miyagi-Do against Cobra Kai.",
+                "name": "Chozen Toguchi",
+                "rivals": "Terry Silver, Kim Da-Eun, Cobra Kai",
+                "friends": "Sato Toguchi, Daniel LaRusso, Johnny Lawrence, Miyagi-Do, Eagle Fang",
+            },
+            "Daniel": {
+                "ID": 18,
+                "bio": "Moved to California from New Jersey in the 80s and would become a target of bullying from Cobra Kai (including Johnny Lawrence). He would eventually be trained in Miyagi-Do by Miyagi himself and would become two time All Valley champion. He would eventually open his own dojo called Miyagi-Do to combat Cobra Kai. Eventually Daniel joins forces with Johnny Lawrence to take on Cobra Kai under Kreese and Silver. Daniel would also seek the help of Chozen to defeat Cobra Kai.",
+                "name": "Daniel LaRusso",
+                "rivals": "John Kreese, Terry Silver, Cobra Kai",
+                "friends": "Johnny Lawrence, Samantha LaRusso, Sato Toguchi, Chozen Toguchi, Miyagi-Do, Eagle Fang",
+            },
+        },
+        "students": {
+            "Samantha": {
+                "ID": 19,
+                "bio": "The daughter of Daniel LaRusso who was taught Miyagi-Do style by Daniel. She would also train with Johnny Lawrence and learn the Eagle Fang style. She competed in the 2019 All Valley losing to Tory in the finals.",
+                "name": "Samantha LaRusso",
+                "rivals": "Tory Nichols, Kyler Park, John Kreese, Kim Da-Eun, Cobra Kai",
+                "friends": "Aisha Robinson, Johnny Lawrence, Chozen Toguchi, Demetri Alexopoulos, Chris, Eli Moskowitz, Robby Keene, Miyagi-Do, Eagle Fang",
+            },
+            "Eli": {
+                "ID": 20,
+                "bio": "A quiet and bullied student who would later join Cobra Kai under Johnny Lawrence. He would compete in one All Valley losing to Robby by disqualification. He would stick with Cobra Kai under Kreese and eventually join Miyagi-Do and win the 2019 All Valley males division.",
+                "name": "Eli Moskowitz",
+                "rivals": "Kyler Park, John Kreese, Terry Silver, Kenny Payne, Cobra Kai",
+                "friends": "Demetri Alexopoulos, Miguel Diaz, Daniel LaRusso, Robby Keene, Samantha LaRusso, Tory Nichols, Chozen Toguchi, Miyagi-Do, Eagle Fang.",
+            },
+            "Demetri": {
+                "ID": 21,
+                "bio": "A quiet and bullied student who would eventually join Miyagi-Do and compete in the 2019 All Valley.",
+                "name": "Demetri Alexopoulos",
+                "rivals": "Kyler Park, Tory Nichols, Terry Silver, John Kreese, Cobra Kai",
+            },
+            "Robby": {
+                "ID": 22,
+                "bio": "The son of Johnny Lawrence who would become trained by Daniel in Miyagi-Do and participate in the 2018 All Valley being runner up to Miguel. He would later join Cobra Kai after being released from Juvie. He would participate in the 2019 All Valley and be runner up to Eli. He then quit Cobra Kai and joined Miyagi-Do",
+                "name": "Robby Keene",
+                "rivals": "John Kreese, Terry Silver, Kyler Park, Kim Da-Eun",
+                "friends": "Miguel Diaz, Eli Moskowitz, Demetri Alexopoulos, Samantha LaRusso, Kenny Payne, Daniel LaRusso, Chozen Toguchi, Tory Nichols",
+            },
+            "Chris": {
+                "ID": 23,
+                "bio": "Joined Cobra Kai originally then switched to Miyagi-Do and participated in the 2019 All Valley.",
+                "name": "Chris",
+                "rivals": "Mitch, John Kreese, Terry Silver. Kyler Park, Kenny Payne, Cobra Kai",
+                "friends": "Demetri Alexopoulos, Nathaniel, Daniel LaRusso, Chozen Toguchi, Johnny Lawrence, Samantha LaRusso, Miguel Diaz, Eli Moskowitz, Robby Keene, Miyagi-Do, Eagle Fang",
+            },
+            "Nathaniel": {
+                "ID": 24,
+                "bio": "Joined Miyagi-Do and competed in the 2019 All Valley",
+                "name": "Nathaniel",
+                "rivals": "John Kreese, Terry Silver, Kenny Payne, Cobra Kai",
+                "friends": "Bert, Chris, Demetri Alexopoulos, Miyagi-Do, Eagle Fang",
+            },
+        },
+    },
+    "WC": {
+        "Mike": {
+            "ID": 26,
+            "bio": "The former Karate Bad Boy during the 80s who was brought in my Terry Silver to help defeat Miyagi and Daniel in the All Valley. He eventually loses to Daniel and ends up being banned from karate. He would go on to work odd jobs until he met his future wife and father in law and would run a furniture store. He would later assist Daniel in stopping Terry Silver from running Cobra Kai. He is a wild card because no one knows which side he would help with.",
+            "name": "Mike Barnes",
+            "rivals": "Terry Silver, John Kreese, Cobra Kai",
+            "friends": "Daniel LaRusso (possibly), Johnny Lawrence (possibly), Chozen Toguchi (possibly)",
+        },
+    },
+}
